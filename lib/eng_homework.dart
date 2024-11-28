e1641337cffa0357c93a0d854a672e808a22623b
Komalanap07@gmail.com <komalanap07@gmail.com>
2024-11-28 04:04:00 -1200

    Your commit message here

--- lib/eng_homework.dart
+++ lib/eng_homework.dart
@@ -15,7 +15,7 @@ class _sub_homeworkState extends State<sub_homework> {
         child: Column(
           children: [
             Container(
-              height: 200,
+              height: 160,
               // width: double.infinity,
               // color: Colors.black87,
               child: Row(
@@ -66,7 +66,7 @@ class _sub_homeworkState extends State<sub_homework> {
                             ),
                           ),
                           Padding(
-                            padding: const EdgeInsets.all(13.0),
+                            padding: const EdgeInsets.all(8.0),
                             child: Text('Submitted',
                                 style: TextStyle(
                                     fontSize: 18, color: Colors.black)),
@@ -97,7 +97,122 @@ class _sub_homeworkState extends State<sub_homework> {
                 child: Column(
                   children: [
                     Container(
-                      height: 100,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(207, 100, 157, 255),
+                      ),
+                      height: 80,
+                      width: double.infinity,
+                      
+                      child: Column(
+                        children: [
+                         Padding(
+                           padding: const EdgeInsets.all(5.0),
+                           child: Text('English(101)',style: TextStyle(fontWeight: FontWeight.bold,),),
+                         ),
+                          Row(
+                            children: [
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(120, 40),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    'view',
+                                    style: TextStyle(fontSize: 17,color: Colors.green),
+                                  )),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(110, 40),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    'submit',
+                                    style: TextStyle(fontSize: 17,color: Colors.red),
+                                  )),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(120, 40),
+                                  ),
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Download',
+                                    style: TextStyle(fontSize: 17,color: Colors.green),
+                                  ))
+                            ]
+                          ),
+                        ],
+                      ),
+              
+                      
+                    ),
+                    Container(
+                      child: Column(
+                        children: [
+                         Text('assign 1',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15)),
+                         Padding(
+                           padding: const EdgeInsets.all(5.0),
+                           child: Row(
+                            children: [
+                              Text('Homework Date'),SizedBox(width: 25,),
+                              Text('04/04/2024')
+                            ],
+                           ),
+                         ),
+                         Padding(
+                           padding: const EdgeInsets.all(5.0),
+                           child: Row(
+                            children: [
+                              Text('Submission Date'),SizedBox(width: 25,),
+                              
+                              Text('15/04/2024')
+                            ],
+                           ),
+                         ),
+                         Padding(
+                           padding: const EdgeInsets.all(5.0),
+                           child: Row(
+                            children: [
+                              Text('Created By'),SizedBox(width: 25,),
+                              Text('Joe Black(9000)')
+                            ],
+                           ),
+                         ),
+                         Padding(
+                           padding: const EdgeInsets.all(5.0),
+                           child: Text('Description',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+                         ),
+                         Padding(
+                           padding: const EdgeInsets.only(bottom: 5.0),
+                           child: Text('Please submit homework before last date.'),
+                         )
+                        ],
+                      ),
+                      
+
+                    ),
+                  
+                  ],
+                ),
+              ),
+            
+            ),
+             Padding(
+              padding: const EdgeInsets.all(13.0),
+              child: Card(
+                elevation: 20, // Controls the shadow depth
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(16), // Sets border radius
+  ),
+
+
+
+
+                color: const Color.fromARGB(238, 255, 255, 255),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 90,
                       width: double.infinity,
                       // color: const Color.fromARGB(207, 100, 157, 255),
                       child: Column(
@@ -147,7 +262,7 @@ class _sub_homeworkState extends State<sub_homework> {
                         children: [
                          Text('assign 1',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15)),
                          Padding(
-                           padding: const EdgeInsets.all(8.0),
+                           padding: const EdgeInsets.all(5.0),
                            child: Row(
                             children: [
                               Text('Homework Date'),SizedBox(width: 25,),
@@ -156,7 +271,7 @@ class _sub_homeworkState extends State<sub_homework> {
                            ),
                          ),
                          Padding(
-                           padding: const EdgeInsets.all(8.0),
+                           padding: const EdgeInsets.all(5.0),
                            child: Row(
                             children: [
                               Text('Submission Date'),SizedBox(width: 25,),
@@ -166,7 +281,7 @@ class _sub_homeworkState extends State<sub_homework> {
                            ),
                          ),
                          Padding(
-                           padding: const EdgeInsets.all(8.0),
+                           padding: const EdgeInsets.all(5.0),
                            child: Row(
                             children: [
                               Text('Created By'),SizedBox(width: 25,),
@@ -175,11 +290,11 @@ class _sub_homeworkState extends State<sub_homework> {
                            ),
                          ),
                          Padding(
-                           padding: const EdgeInsets.all(8.0),
+                           padding: const EdgeInsets.all(5.0),
                            child: Text('Description',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
                          ),
                          Padding(
-                           padding: const EdgeInsets.only(bottom: 10.0),
+                           padding: const EdgeInsets.only(bottom: 5.0),
                            child: Text('Please submit homework before last date.'),
                          )
                         ],
